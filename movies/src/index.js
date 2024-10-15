df1a051c28b8e1220eff435fd34135e077fdbc2e
Peter Woods <woodspeter37@gmail.com>
2024-10-15 14:21:16 +0100

    Fix backward/forward arrows

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Navigate, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
@@ -8,16 +9,16 @@ import SiteHeader from './components/siteHeader'
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <SiteHeader />
-    <Routes>
-      <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-      <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-      <Route path="/movies/:id" element={<MoviePage />} />
-      <Route path="/" element={<HomePage />} />
-      <Route path="*" element={ <Navigate to="/" /> } />
-    </Routes>
-  </BrowserRouter>
+     <BrowserRouter>
+      <SiteHeader />
+      <Routes>
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+        <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={ <Navigate to="/" /> } />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
